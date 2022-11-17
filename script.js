5aece14f100982768ef4743cc8bec84b637a7491
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2022-11-16 20:35:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Where we store objects -> array full of objects that are recieved
 let apiQuotes = [];
 
+// Show New Quote
+function newQuote() {}
+
 async function getQuotes() {
   //fetching a source and storing it
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl); //once {won't exist unless something is recieved} source is received, that is label as our response.
     apiQuotes = await response.json();
-    console.log(apiQuotes[1]);
+    newQuote();
     //breaking that json into objects;
   } catch (error) {
     alert(error);
