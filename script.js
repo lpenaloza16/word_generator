835a70bf1e8ef77a54e2dd46d2bdd11665686226
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2022-11-16 19:09:32 -0800

    Working with the fetch api
    collecting quote data

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const loader = document.getElementById('loader');;
+// Where we store objects -> array full of objects that are recieved
+let apiQuotes = [];
 
-//loader function
-function loading () {
-    loader.hidden = false;
-    quote
+async function getQuotes() {
+  //fetching a source and storing it
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl); //once {won't exist unless something is recieved} source is received, that is label as our response.
+    apiQuotes = await response.json();
+    console.log(apiQuotes[1]);
+    //breaking that json into objects;
+  } catch (error) {
+    alert(error);
+    //handle error
+  }
 }
+
+// On load
+
+getQuotes();
