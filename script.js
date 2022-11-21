a98b91bb49de4c12b16e9862808167cd04d6a732
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2022-11-21 15:15:41 -0800

    Adding in The Math function

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 let apiQuotes = [];
 
 // Show New Quote
+
+function getRandomInt(max) {
+  console.log(Math.floor(Math.random() * Math.floor(max)));
+}
+
 function newQuote() {}
 
 async function getQuotes() {
@@ -9,8 +14,10 @@ async function getQuotes() {
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl); //once {won't exist unless something is recieved} source is received, that is label as our response.
-    apiQuotes = await response.json();
+    apiQuotes = await response.json(); //destructs the JSON to Objects; extracted
     newQuote();
+    // console.log(apiQuotes[12]); //reads objects
+
     //breaking that json into objects;
   } catch (error) {
     alert(error);
@@ -20,4 +27,4 @@ async function getQuotes() {
 
 // On load
 
-getQuotes();
+getQuotes(); //perform
